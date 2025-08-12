60012c7d98cbf7c26df0546246471b7df5769c6b
Dajuan <107205602+TheBrownBadger@users.noreply.github.com>
2025-08-12 11:33:31 -0400

    Update and rename wifi_signal.sh to wifi_clients.sh

---
+++ telegraf/wifi_clients.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# wifi_clients.sh - WiFi station list for InfluxDB
+# Requires: iw
+
+HOST=$(hostname)
+IFACE="wlan0"
+
+sudo iw dev "$IFACE" station dump | \
+awk -v host="$HOST" '
+/Station/ {MAC=$2}
+/signal:/ {SIG=$2; printf "wifi_clients,host=%s,mac=%s signal_dbm=%s\n", host, MAC, SIG}'
+
+# Make it executable
+`chmod +x /home/pi/scripts/wifi_clients.sh`
+
