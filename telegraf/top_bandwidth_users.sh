8687f4ed02a34cfad2c33e01a83a4fab746c4ee7
Dajuan <107205602+TheBrownBadger@users.noreply.github.com>
2025-08-12 11:34:11 -0400

    Create top_bandwidth_users.sh

---
+++ telegraf/top_bandwidth_users.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# top_bandwidth_users.sh - Top talkers for InfluxDB
+# Requires: iftop
+
+HOST=$(hostname)
+IFACE="wlan0" # AP interface
+
+sudo iftop -t -s 10 -i "$IFACE" 2>/dev/null \
+| grep -E "=>" \
+| awk '{print $2,$3}' \
+| head -n 5 \
+| while read IP BYTES; do
+    CLEAN_IP=$(echo $IP | tr -d '[:space:]')
+    CLEAN_BYTES=$(echo $BYTES | tr -d '[:alpha:]')
+    echo "top_users,host=$HOST,ip=$CLEAN_IP bytes=$CLEAN_BYTES"
+done
