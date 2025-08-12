d824fabcecb44095d7bd91b7afec7656cd4c1ed2
Dajuan <107205602+TheBrownBadger@users.noreply.github.com>
2025-08-12 11:35:35 -0400

    Create vnstat_summary.sh

---
+++ telegraf/vnstat_summary.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# vnstat_summary.sh - Bandwidth usage per interface in InfluxDB line protocol
+# Requires: vnstat, jq
+
+HOST=$(hostname)
+
+vnstat --json | jq -c '.interfaces[]' | while read iface; do
+    NAME=$(echo "$iface" | jq -r '.name')
+    RX=$(echo "$iface" | jq -r '.traffic.day[-1].rx') # bytes received today
+    TX=$(echo "$iface" | jq -r '.traffic.day[-1].tx') # bytes sent today
+
+    echo "vnstat_usage,host=$HOST,interface=$NAME rx_bytes=$RX,tx_bytes=$TX"
+done
